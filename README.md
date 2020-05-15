dd195cd863d6dc380e9627d0cbab5b8cb52f0887
Mark Pearce <mark.pearce@redspace.com>
2020-05-15 13:28:00 -0300

    Able to just specify new brand for files to be generated

--- README.md
+++ README.md
@@ -25,8 +25,10 @@ Options for RokuBuilder::Generator:
 Commands:
         --generate COMPONENT_TYPE    Generate a component: manager, module, task, screen
         --name COMPONENT_NAME        Name of the component
+Options:
         --extends COMPONENT_NAME     A component to extend
-        --base-dir                   Base directory for generated code (eg.: 'brands/core/components/<type>s')
+        --for-brand BRAND            Brand to put component in (default:' core')
+        --base-dir                   Base directory for generated brs/xml code (eg.: 'brands/core/components/<type>s')
         --with-config                Add empty config JSON
         --config-dir                 Use custom directory for config json (eg.: 'brands/core/region/US/configs/<type>s')
 ```
@@ -49,11 +51,44 @@ Format of config file:
 
 ```js
 {
-  "component_dir": "base/directory/for/components", // default: "./brands/core/components"
-  "config_dir": "base/directory/for/config/json/files", // default: "./brands/core/region/US/configs"
+  "component_dir": "base/directory/for/components", // default: "./brands/%brand%/components"
+  "config_dir": "base/directory/for/config/json/files", // default: "./brands/%brand%/region/US/configs"
   "template_dir": "directory/for/templates" // default: "./roku_builder_generator"
 }
 ```
+
+### Directories and Brands
+
+Unless you specify a custom directory, generated files will be placed in the default directory. For example,
+`roku --generate manager --name foo --with-config`
+will generate the following files:
+
+- ./brands/core/components/Managers/FooManager/FooManager.xml
+- ./brands/core/components/Managers/FooManager/FooManager.brs
+- ./brands/core/region/US/configs/Managers/Foo.json
+
+without specifying custom directories, files can be generated for other brands. For example,
+
+`roku --generate manager --name foo --for-brand comedy --with-config`
+
+will generate:
+
+- ./brands/comedy/components/Managers/FooManager/FooManager.xml
+- ./brands/comedy/components/Managers/FooManager/FooManager.brs
+- ./brands/comedy/region/US/configs/Managers/Foo.json
+
+To enable changing brands for directories specified in `.roku_builder_generator.json`, use `%brand%` in the position where the brand should be replaced.
+
+For example, if you specify `"component_dir": "some/directory/%brand%/components"` and you ran the command
+
+`roku --generate manager --name foo --for-brand my-brand`
+
+it would generate files
+
+- ./some/directory/my-brand/components/Managers/FooManager/FooManager.xml
+- ./some/directory/my-brand/components/Managers/FooManager/FooManager.brs
+
+Note: If nothing is specified by the `--for-brand` option, it will use "core".
 
 ### Custom Templates
 
--- lib/roku_builder/plugins/generator.rb
+++ lib/roku_builder/plugins/generator.rb
@@ -5,22 +5,29 @@ require_relative '../../roku_builder_generator'
 
 def default_config()
   return {
-    :component_dir => "brands/core/components",
-    :config_dir => "brands/core/region/US/configs",
+    :component_dir => "brands/%brand%/components",
+    :config_dir => "brands/%brand%/region/US/configs",
     :template_dir => "roku_builder_generator"
   }
 end
 
-def default_output_dir(component_type, base_dir = nil)
+def default_output_dir(component_type, brand = 'core', base_dir = nil)
   base_dir = base_dir || default_config[:component_dir]
+  base_dir = replace_brand(base_dir, brand)
   return "#{base_dir}/#{component_type.capitalize()}s"
 end
 
-def default_config_dir(component_type, base_dir = nil)
+def default_config_dir(component_type, brand = 'core', base_dir = nil)
   base_dir = base_dir || default_config[:config_dir]
-   return "#{base_dir}/#{component_type.capitalize()}s"
+  base_dir = replace_brand(base_dir, brand)
+  return "#{base_dir}/#{component_type.capitalize()}s"
 end
 
+
+def replace_brand(directory, newBrand = 'core')
+  directory["%brand%"]=newBrand
+  return directory
+end
 
 module RokuBuilder
 
@@ -43,20 +50,19 @@ module RokuBuilder
     end
 
 
-    def get_output_dir( component_type, component_name, parent_dir = nil, custom_dir = nil, base_component_dir = nil )
+    def get_output_dir( component_type, component_name, brand, parent_dir = nil, custom_dir = nil, base_component_dir = nil )
       output_dir = get_directory_name(component_type, component_name)
       unless parent_dir.nil? || "" === parent_dir
         output_dir = parent_dir+"/"+output_dir
       end
-      base_dir = custom_dir ? custom_dir : default_output_dir(component_type, base_component_dir)
+      base_dir = !custom_dir.nil? ? replace_brand(custom_dir, brand) : default_output_dir(component_type, brand, base_component_dir)
       return "./#{base_dir}/#{output_dir}"
 
     end
 
-    def get_config_output_dir( component_type, custom_dir = nil, base_config_dir = nil )
-      base_dir = custom_dir ? custom_dir : default_config_dir(component_type, base_config_dir)
+    def get_config_output_dir( component_type, brand, custom_dir = nil )
+      base_dir = !custom_dir.nil? ? replace_brand(custom_dir, brand) : default_config_dir(component_type, brand)
       return "./#{base_dir}"
-
     end
 
     # Hook to add options to the parser
@@ -64,16 +70,21 @@ module RokuBuilder
     #   hash
     def self.parse_options(parser:,  options:)
       parser.separator "Commands:"
+      options[:brand] = 'core'
       parser.on("--generate COMPONENT_TYPE", "Generate a component: manager, module, task, screen") do |component_type|
         options[:generate] = component_type
       end
       parser.on("--name COMPONENT_NAME", "Name of the component") do |component_name|
         options[:name] = component_name
       end
+      parser.separator "Options:"
       parser.on("--extends COMPONENT_NAME", "A component to extend") do |component_name|
        options[:extends] = component_name
       end
-      parser.on("--base-dir", "Base directory for generated code (eg.: '#{default_output_dir('<type>')}')") do |base_dir|
+      parser.on("--for-brand BRAND", "Brand to put component in (default:' core')") do |brand|
+        options[:brand] = brand
+      end
+      parser.on("--base-dir", "Base directory for generated brs/xml code (eg.: '#{default_output_dir('<type>')}')") do |base_dir|
         options[:custom_dir] = base_dir
       end
       parser.on("--with-config", "Add empty config JSON") do |d|
@@ -96,9 +107,11 @@ module RokuBuilder
       #Setup
     end
 
+    def capitalizeFirst(name)
+      name.slice(0,1).capitalize + name.slice(1..-1)
+    end
 
     def get_file_name(component_type, name)
-      name = name.slice(0,1).capitalize + name.slice(1..-1)
       if(component_type == 'screen' && !name.end_with?("Screen"))
         return name+ "Screen"
       end
@@ -109,7 +122,6 @@ module RokuBuilder
     end
 
     def get_directory_name(component_type, name)
-      name = name.slice(0,1).capitalize + name.slice(1..-1)
       if(component_type === 'manager'&& !name.end_with?("Manager"))
         return name+'Manager'
       end
@@ -131,8 +143,9 @@ module RokuBuilder
       config = read_config()
       component_type = options[:generate].downcase
       component_name_parts = options[:name].split('/')
-      component_proper_name = component_name_parts.last
+      component_proper_name = capitalizeFirst(component_name_parts.last)
       component_parent_dir = component_name_parts.first(component_name_parts.size-1).join('/')
+      brand = brand.downcase
 
       component_name = get_file_name(component_type, component_proper_name)
       component = RokuBuilderGenerator::BrsComponent.new(component_name, options[:extends], component_type, @logger)
@@ -140,8 +153,8 @@ module RokuBuilder
       brs_text = component.render("brs")
       json_text = options[:with_config] && component_has_config_json(component_type) ? component.render("json") : nil
 
-      output_dir = get_output_dir(component_type, component_name, component_parent_dir, options[:custom_dir], config[:component_dir])
-      output_config_dir = get_config_output_dir(component_type, options[:config_dir], config[:config_dir])
+      output_dir = get_output_dir(component_type, component_name, brand, component_parent_dir, options[:custom_dir], config[:component_dir])
+      output_config_dir = get_config_output_dir(component_type,  brand, options[:config_dir])
       output_file_name = File.join(output_dir, component_name)
       output_config_file_name = File.join(output_config_dir, component_proper_name)
 
@@ -176,7 +189,11 @@ module RokuBuilder
     def write_file(output_file_name, contents)
       unless contents.nil?
         @logger.unknown output_file_name
-        File.open(output_file_name, "w") { |f| f.write contents }
+        if File.exist?(output_file_name)
+          @logger.warn "#{output_file_name} already exists, skipping"
+        else
+          File.open(output_file_name, "w") { |f| f.write contents }
+        end
       end
     end
 
